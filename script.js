1b5de460bf07e32f211a71c37ad9a39f1ac781cc
shinichi-cmd <Academico@T08KETIPC08C203>
2024-10-04 11:29:33 -0500

    actualizacion 4

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function link(){
     Swal.fire({
-        imageUrl: "https://placeholder.pics/svg/300x1500",
-        imageHeight: 1500,
+        imageUrl: "https://media.canalnet.tv/2021/01/Screen-Shot-2021-01-06-at-5.15.29-PM.png",
         imageAlt: "A tall image"
       });
 }
